3c10fe97ed6b197877b33c767b0b6186b11a28a0
Jeong-wonho <kallzzang@naver.com>
2021-11-27 18:09:21 +0900

    공유객체를 사용할 때의 주의점

--- thisisjava/chap12/src/chap12/MainThreadExample.java
+++ thisisjava/chap12/src/chap12/MainThreadExample.java
@@ -1,5 +1,20 @@
 package chap12;
 
+/**
+ * 공유객체 사용할 때 주의 할 점!
+ * @author X13
+ *
+ */
 public class MainThreadExample {
-
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		User1 user1 = new User1();
+		user1.setCalculator(calculator);
+		user1.start();
+		
+		User2 user2 = new User2();
+		user2.setCalculator(calculator);
+		user2.start();
+	}
 }
