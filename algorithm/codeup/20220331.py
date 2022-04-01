9942862034c41d28e4ea78f52b2dba8904421f63
Jeong-wonho <kallzzang@naver.com>
2022-04-01 23:39:45 +0900

    20220401 커밋

---
+++ algorithm/codeup/20220331.py
@@ -0,0 +1,19 @@
+#6046
+# n = input()
+# print(int(n) << 1)
+
+#6047
+a, b = input().split()
+print(int(a) << int(b))
+
+#6048
+a, b = input().split()
+print(int(a) < int(b))
+
+#6049
+a, b = input().split()
+print(int(a) == int(b))
+
+#6050
+a, b = input().split()
+print(int(a) <= int(b))
