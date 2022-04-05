bc71dbe8b027c65da322763154c19022915ed600
Jeong-wonho <kallzzang@naver.com>
2022-04-05 23:54:25 +0900

    비트단위논리연산

---
+++ algorithm/codeup/20220405.py
@@ -0,0 +1,4 @@
+#6059 비트단위논리연산
+#** 비트단위(bitwise) 연산자는, ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor), <<(bitwise left shift), >>(bitwise right shift) 가 있다.
+s = int(input())
+print(~s)
