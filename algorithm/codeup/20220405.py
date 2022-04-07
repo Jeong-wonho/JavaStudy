02ae766dcbb2896f07415564e7d034c4a47342dd
Jeong-wonho <kallzzang@naver.com>
2022-04-07 23:11:10 +0900

    삼항연산

--- algorithm/codeup/20220405.py
+++ algorithm/codeup/20220405.py
@@ -22,5 +22,5 @@
 #6064 삼항연산 
 a, b, c = input().split()
 a = int(a); b = int(b); c = int(c);
-d = (a if a>b else b) if ((a if a > b else b) > c) else c
+d = (a if a<b else b) if ((a if a < b else b) < c) else c
 print(d);
