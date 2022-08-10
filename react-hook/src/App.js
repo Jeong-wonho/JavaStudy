f252598fbd717bc9f30c8945d138b7c57dc94b46
Jeong-wonho <kallzzang@naver.com>
2022-08-10 20:38:04 +0900

    useRef

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,8 +1,25 @@
 import UserList from "./components/UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
   return (
-    <UserList/>
+    <UserList users={users}/>
   );
 }
 
--- react-hook/src/components/UserList.js
+++ react-hook/src/components/UserList.js
@@ -1,33 +1,17 @@
 function User({user}) {
-    return (
+    return(
         <div>
             <b>{user.username}</b><span>({user.email})</span>
         </div>
     )
 }
 
-export default function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
-
+export default function UserList({users}) {
     return (
         <div>
-            {users.map((user) => <User user={user} key={user.id}/>)}
+            {users.map(user => (
+                <User user={user} key={user.id} />
+            ))}
         </div>
     )
 }
