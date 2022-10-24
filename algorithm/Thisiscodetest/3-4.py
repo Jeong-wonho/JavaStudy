e95b1c5d0f79d4933b9272a6bf7aa65f8e142629
Jeong-wonho <kallzzang@naver.com>
2022-10-24 23:26:12 +0900

    greedy 문제 3과

---
+++ algorithm/Thisiscodetest/3-4.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split());
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수 '찾기
+    min_value = 10001 #10000이하이기 때문에 10001로 설정
+    for a in data:
+        min_value = min(min_value, a);
+    
+    result = max(result, min_value);
+print(result);
--- algorithm/Thisiscodetest/greedy1.py
+++ algorithm/Thisiscodetest/greedy1.py
@@ -6,20 +6,13 @@ data.sort() #입력받은 수들 정렬하기
 first = data[n-1]
 second = data[n-2]
 
+count = int(m/(k+1)) * k #가장 큰 수가 반복되는 횟수
+count += m %(k+1) # 나머지가 있을 수 있는 경우
+
 result = 0
+result += (count) * first
+result += (m-count) * second
 
-while True:
-    for i in range(k): # 가장 큰 수를 K번 더하기
-        if m == 0:#m이 0이라면 반복문 탈출
-            break
-        result += first
-        m -= 1 #더할 때마다 1씩 빼기
-    if m == 0: #m이 0이라면 반복문 탈출
-        break
-    result += second #두번째로 큰 수를 한 번 더하기
-    m -= 1 # 더할 때마다 1씩 빼기
-
-print(result);
-
+print(result)
 
             
